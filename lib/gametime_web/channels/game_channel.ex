82994059d08d257bc458e121633543742c1082e9
Andreas Knöpfle <andi@bitcrowd.net>
2019-04-09 21:29:06 +0200

    fix calling game master

--- lib/gametime_web/channels/game_channel.ex
+++ lib/gametime_web/channels/game_channel.ex
@@ -3,16 +3,20 @@ defmodule GametimeWeb.GameChannel do
 
   def join("game:" <> game_name, %{"name" => name}, %{assigns: %{id: player_id}} = socket) do
     :ok = GametimeWeb.Endpoint.subscribe("player:" <> player_id)
-    GameMaster.join(String.to_existing_atom(game_name), SocketPlayer.new(player_id, name))
+
+    String.to_existing_atom(game_name)
+    |> GameMaster.join(SocketPlayer.new(player_id, name))
+
     {:ok, socket}
   end
 
   def handle_in(
         "act",
         %{"actions" => actions},
-        %{topic: "game" <> name, assigns: %{id: player_id}} = socket
+        %{topic: "game" <> game_name, assigns: %{id: player_id}} = socket
       ) do
-    GameMaster.act(name, player_id, actions)
+    String.to_existing_atom(game_name)
+    |> GameMaster.act(player_id, actions)
 
     {:reply, {:thanks, %{}}, socket}
   end
